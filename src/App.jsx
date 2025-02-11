4510b993c2e776abe50dbd6eb0b3e28b95ce522a
Yoel Peter Chandra <yoelpc4@gmail.com>
2025-02-12 00:02:30 +0700

    fix: update is accepted image

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,11 @@ function App() {
     <div className="flex flex-col items-center justify-center text-center gap-10 p-10 h-screen bg-pink-200">
       {isAccepted ? (
         <>
-          <svg
-            className="size-64 fill-pink-500 animate-bounce"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
-                2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81
-                14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55
-                11.54L12 21.35z"
-            />
-          </svg>
+          <img
+            src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExazF2eWRqdTkwMWIwcjc0NXl3MW4zNG13a2EyNjB1YXR2bGtydjE1byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IzXiddo2twMmdmU8Lv/giphy.gif'
+            alt='love'
+            width="300"
+          />
           <h1 className="text-3xl font-bold text-pink-500">
             Yaay <span className="animate-pulse">😘</span>
           </h1>
